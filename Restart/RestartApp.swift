2b852f9db749df0a40bb6dbc005c829bcef30873
jperez1478 <jessicperez3@gmail.com>
2021-11-16 20:11:28 -0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jessica Perez on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
