f6962d2f09a7e72c59dd6c54db959746bf0a9e23
jperez1478 <jessicperez3@gmail.com>
2021-11-26 15:41:30 -0600

    complete view

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -14,18 +14,58 @@ struct OnboardingView: View {
     //MARK: -Body
     var body: some View {
         ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             VStack(spacing: 20) {
-                Text("Onboarding")
-                    .font(.largeTitle)
-                Button(action:  {
-                    isOnboardingViewActive = false
-                }) {
-                    Text("Start")
-                }
+               //MARK: -Header
+                
+                Spacer()
+                VStack(spacing: 20) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                        
+                        It's not how much we give but
+                        How much love we put into giving
 
+                        """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }
+                
+                //MARK: -Center
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }  //:ZStack
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }//:Center
+                
+                Spacer()
+                
+                //MARK: -Footer
+                
+                
+        
             }
         }//: VStack
-    }
+    }//: ZStack
 }
 
 struct OnboardingView_Previews: PreviewProvider {
