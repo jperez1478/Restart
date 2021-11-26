1f9bf0cdc3507e660a302e50d6bf7bdb6c772017
jperez1478 <jessicperez3@gmail.com>
2021-11-26 12:20:18 -0600

    onboarding views work

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,16 +13,18 @@ struct OnboardingView: View {
     
     //MARK: -Body
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            Button(action:  {
-                isOnboardingViewActive = false 
-            }) {
-                Text("Start")
-            }
+        ZStack {
+            VStack(spacing: 20) {
+                Text("Onboarding")
+                    .font(.largeTitle)
+                Button(action:  {
+                    isOnboardingViewActive = false
+                }) {
+                    Text("Start")
+                }
 
-        }//: VStack 
+            }
+        }//: VStack
     }
 }
 
